3720db28b0be9ce8aa0e210888f947df07db8a17
Sayon Mazumder <Sayon Mazumder>
2017-02-06 09:02:09 +0530

    server.js upload

--- server.js
+++ server.js
@@ -10,15 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("article one is here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.send("article two is here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three', function (req, res) {
-  res.send("article three is here");
-});
+
 
 
 app.get('/ui/style.css', function (req, res) {
