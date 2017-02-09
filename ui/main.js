230a6b39c18569b54254d48e8ca26f517119d44a
sayan2107 <sayan21071995@gmail.com>
2017-02-10 02:08:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,29 @@
-console.log('Loaded!');
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+//create a request to counter endpoint
+var request = new XMLHttpRequest();
 
 
-//move image
-var img=document.getElementById('madi');
-img.onclick=function(){
- img.style.marginLeft = '200px';
+//capture the request and store it inside a variable
+request.onreadystatechange = function(){
+if(request.readyState===XMLHttpRequest().DONE){
+//TAKE ACTION
+ if(request.status===200){  //checking if it is a succesfull request or not
+       var counter=request.responseText;
+        counter=counter+1;
+        document.getElementById('count').innerHTML = counter.toString();
+        }
+     }
+
+//later  else part
+
+  };
+  
+//make a request
+
+request.open('GET','http://sayan2107.imad.hasura-app.io/counter',true);
+rewuest.send(null);
 
 };
