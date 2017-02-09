25736aa0798b42dee4fa89ae99fe12990fd3109b
sayan2107 <sayan21071995@gmail.com>
2017-02-09 23:30:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 //changing inner html with ID 'bla'
 document.getElementById('bla').innerHTML='new valuie';
-// alert("value changed");
+
 
 
 //move image
 var img=document.getElementById('madi');
-img.onClick=function(){
+img.onclick=function(){
  img.style.marginleft = '300px';
 };
