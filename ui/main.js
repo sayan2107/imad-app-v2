f842785dc2e1bb3acfb0ba1e2206202a9d84820c
sayan2107 <sayan21071995@gmail.com>
2017-02-10 19:51:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick = function(){
 //create a request to counter endpoint
 var request = new XMLHttpRequest();
@@ -10,20 +10,62 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function(){
 if(request.readyState===XMLHttpRequest.DONE){
 //TAKE ACTION
- if(request.status===200){  //checking if it is a succesfull request or not
-       var counter=request.responseText;
+ if(request.status===200){               //checking if it is a succesfull request or not
+        counter=0;                         
+        var counter=request.responseText;
+        
         counter=counter+1;
         document.getElementById('count').innerHTML = counter.toString();
         }
      }
 
-     //later  else part
+//later  else part
 
   };
   
 //make a request
 
-request.open('GET','http://sayan2107.imad.hasura-app.io/counter',true);
+request.open('GET','http://localhost:8080/counter',true);
+request.send(null);
+
+};
+
+
+
+
+
+
+//submit names
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+var request = new XMLHttpRequest();  //request to the server
+//capture the request and store it inside a variable
+request.onreadystatechange = function(){
+if(request.readyState===XMLHttpRequest.DONE){
+//TAKE ACTION
+ if(request.status===200){               //checking if it is a succesfull request or not
+       //capture the list of names and render it as a list
+
+var names = request.responseText;
+names = JSON.parse(names);
+var list="";
+for(var i=0; i<names.length; i++){
+    list += '<li>' + names[i] + '</li>';
+ }
+ var ul = document.getElementById('namelist');
+ ul.innerHTML = list;
+        }
+     }
+
+//later  else part
+
+  };
+  
+//make a request
+var nameinput=document.getElementById('name');
+var name = nameinput.value;
+request.open('GET','http://localhost:8080/submit-name?name=' + name,true);
 request.send(null);
 
 };
